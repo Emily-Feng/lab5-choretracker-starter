67151fbde0a66dc923c3160a6073b2bc126fa06d
Emily Feng <manqingf@andrew.cmu.edu>
2021-03-02 21:37:19 -0500

    add to task

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,4 +3,23 @@ class Chore < ApplicationRecord
     # Relationships
     belongs_to :child
     belongs_to :task
+
+    # Validations
+    validates_date :due_on
+
+    # Scopes
+    scope :by_task, -> { joins(:task).order('tasks.name') }
+    scope :chronological, -> { order('due_on') }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where("due_on >= ?", Date.current) }
+    scope :past, -> { where("due_on < ?", Date.current) }
+    
+    # Methods
+    def status
+        return "Completed" unless !self.completed
+
+        "Pending"
+    end
+
 end
