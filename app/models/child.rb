7eaaa44381a94cec292b92534e71c92d495ba445
Emily Feng <manqingf@andrew.cmu.edu>
2021-03-02 20:32:41 -0500

    add to child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,10 @@
 class Child < ApplicationRecord
-    
+
+    # Relationships
+    have_many(:chores)
+    have_many(:tasks).through(:chores)
+
+    # Validations
+    validate_presence_of :first_name, :last_name
+
 end
